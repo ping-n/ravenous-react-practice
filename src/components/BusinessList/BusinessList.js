4dae41ae6703e605286783c2f16d56fbfeccd71e
PING NGE <ping.nge@hotmail.com>
2020-12-06 21:45:48 +1100

    UPDATE: Created Business and BusinessList components

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./BusinessList.css ";
+import Business from "../Business/Business";
+
+export class BusinessList extends Component {
+  render() {
+    return <Business />;
+  }
+}
+
+export default BusinessList;
